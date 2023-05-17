8cafbd9a894a09d05cf5af5dbb9c8fda26b3a065
PhilBoa <philicbrefo98@gmail.com>
2023-05-17 11:00:21 -0700

    Correcting errors

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """
+    replaces all occurances of search with replace
+    """
+    new_list = my_list
+    for idx, item in enumerate(new_list):
+        if item == search:
+            new_list[idx] = replace
+    return (new_list)
