6a092de91ce72b73bbd7674a40a29d11cd0cf1a3
PhilBoa <philicbrefo98@.gmail.com>
2023-06-13 19:18:09 -0700

    Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -4,7 +4,7 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """Function that insert text after each line containing a given string in a file.
+    """Insert text after each line containing a given string in a file.
 
     Args:
         filename (str): The name of the file.
