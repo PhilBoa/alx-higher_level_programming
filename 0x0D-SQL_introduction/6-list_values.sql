851b8c199f462285e43ed09640aa070bc18bf5e0
PhilBoa <philicbrefo98@gmail.com>
2023-07-18 14:51:29 -0700

    6-list_values.sql

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,6 @@
+-- A script that lists all rows of the table first_table from
+-- the database hbtn_0c_0 in a MySQL server
+
+-- SELECT statement is used for displaying the rows of a table
+SELECT *
+FROM `first_table`;
