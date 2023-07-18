cc64fa0dc4becf0d3cf5493162355f13ea004c4c
PhilBoa <philicbrefo98@gmail.com>
2023-07-18 14:49:24 -0700

    5-full_table.sql

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,8 @@
+-- A script that prints the full description of the table first_table
+-- from the database hbtn_0c_0 in your MySQL server
+
+-- SHOW CREATE TABLE statement is used for display the 
+-- complete CREATE TABLE statement, including the table name,
+-- column definitions, data types, constraints, and any other
+-- options specified during the table creation
+SHOW CREATE TABLE first_table;
