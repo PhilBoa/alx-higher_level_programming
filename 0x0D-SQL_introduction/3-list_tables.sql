2ba4f825b0312a5f1a411cb6d2a21b998ab1edca
PhilBoa <philicbrefo98@gmail.com>
2023-07-19 09:14:42 -0700

    3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,6 +1,2 @@
--- Script that lists all tables in a passed mysql database
-
--- SELECT keyword is used to list tables in a mysql database
-SELECT TABLE_NAME
-FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = DATABASE();
+-- This shows tables in current database
+SHOW TABLES;
