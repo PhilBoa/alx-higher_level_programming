a121f2309b27ffa3936a96dd4e5316b7db644445
PhilBoa <philicbrefo98@gmail.com>
2023-08-20 13:47:55 -0700

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+This script connects to a MySQL database and retrieves states with names
+starting with 'N'.
+"""
+
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    """
+    Main function that connects to the MySQL database and retrieves states
+    with names starting with 'N'.
+    """
+    if len(argv) != 4:
+        print("Usage: {} <username> <password> <db_name>".format(argv[0]))
+        return
+    user = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    con = None
+    cursor = None
+    try:
+        con = MySQLdb.connect(
+                host="localhost",
+                port=3306, user=user, password=password, db=db_name)
+        cursor = con.cursor()
+        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except MySQLdb.Error as e:
+        print("Error connecting to the MySQL server: ", e)
+    finally:
+        if cursor:
+            cursor.close()
+        if con:
+            con.close()
+
+
+if __name__ == "__main__":
+    main()
