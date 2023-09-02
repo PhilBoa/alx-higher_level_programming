0514fe9041a420b6d47faadec80a5f475e115388
PhilBoa <philicbrefo98@gmail.com>
2023-09-01 19:35:58 -0700

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Python script that sends in a URL, and displays the value of the X-Request-Id header.
+"""
+
+import urllib.request
+import sys
+
+
+if len(sys.argv) != 2:
+    print("Usage: python script.py <URL>")
+    sys.exit(1)
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+	request_id = response.getheader("X-Request-Id")
+            if request_id:
+                print(request_id)
+    except Exception as e:
+        sys.stderr.write("Error: {}\n".format(str(e)))
+        sys.exit(1)
