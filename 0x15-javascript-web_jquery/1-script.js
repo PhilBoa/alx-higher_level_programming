68b59acda7fec88cfd7208bfb7218c136d2ea9be
PhilBoa <philicbrefo98@gmail.com>
2023-10-04 17:03:11 +0000

    1-script.js

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Add event listner and Wait for the DOM content to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+  // Update the text color of the header element to red (#FF0000)
+  $('header').css('color', '#FF0000');
+});
