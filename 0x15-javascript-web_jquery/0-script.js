45bf0c50042b8c92afe074d2462d91b16d4c7deb
PhilBoa <philicbrefo98@gmail.com>
2023-10-04 17:00:54 +0000

    0-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,8 @@
+// Add event listner and Wait for the DOM content to be fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the header element using document.querySelector
+  const header = document.querySelector('header');
+
+  // Update the text color of the header element to red (#FF0000)
+  header.style.color = '#FF0000';
+});
